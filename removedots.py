c731802ef5857bb533024def0244dc5c11ec18d4
Luis Guilherme <lgsguirardelli@gmail.com>
2023-09-16 23:11:19 -0300

    traduzido para PT-BR

--- removedots.py
+++ removedots.py
@@ -13,29 +13,29 @@ class CleanNumberApp(App):
     def build(self):
         self.b = BoxLayout(orientation='vertical')
 
-        self.label1 = Label(text='Enter the document number: ')
+        self.label1 = Label(text='COLE O NÚMERO DO DOCUMENTO: ')
         self.b.add_widget(self.label1)
 
         self.textinput = TextInput(multiline=False)
         self.b.add_widget(self.textinput)
 
-        self.label2 = Label(text='Cleaned Number: ')
+        self.label2 = Label(text='NÚMERO LIMPO: ')
         self.b.add_widget(self.label2)
 
         self.result = Label(text='', font_size=32)
         self.b.add_widget(self.result)
 
-        self.button = Button(text='Clean Number')
+        self.button = Button(text='LIMPAR NÚMERO')
         self.button.bind(on_press=self.clean)
         self.b.add_widget(self.button)
 
-        self.button_copy = Button(text='COPY')
+        self.button_copy = Button(text='COPIAR')
         self.button_copy.bind(on_press=self.copy_to_clipboard)
         self.b.add_widget(self.button_copy)
 
         return self.b
     
-    
+
     def clean(self, instance):
         raw_number = self.textinput.text
         cleaned_number = clean_number(raw_number)
